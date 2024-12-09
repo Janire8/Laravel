dbbd9b5861285ed0c60a35f94df41ba01b321111
Janire8 <146227893+Janire8@users.noreply.github.com>
2024-12-09 09:16:05 +0000

    visualizar en la vista elementos de la base de datos

--- example-app/app/Http/Controllers/NoteController.php
+++ example-app/app/Http/Controllers/NoteController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Note;
 
 class NoteController extends Controller
 {
     public function index(){
-        return "hola";
+        // return "hola";
+        $notes = Note::all();
+   
+        
+        return view ('note.index', compact('notes'));
     }
 }
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
+use App\Models\Note;
 
 Route::get('/', function () {
     return view('welcome');
